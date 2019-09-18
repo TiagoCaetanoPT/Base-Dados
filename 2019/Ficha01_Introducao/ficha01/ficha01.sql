7f75395b6a29f057a440d53d6cda016b39fba54b
unknown <tiago.o.caetano@gmail.com>
2019-09-18 13:34:39 +0100

    Fim da Aula - Ficha 01

---
+++ 2019/Ficha01_Introducao/ficha01/ficha01.sql
@@ -0,0 +1,11 @@
+-- opções de ambiente do SQL*PLUS: não são comandos SQL
+SET PAGESIZE 100;
+SET LINESIZE 100;
+SET SQLPROMPT FICHA1> ;
+
+-- execução dos scripts da ficha 1
+@@ ficha01_drops
+@@ ficha01_tabelas
+@@ ficha01_dados
+
+
