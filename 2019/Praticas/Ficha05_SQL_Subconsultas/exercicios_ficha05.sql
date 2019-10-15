b49afe454fa5bf80999093d4996762449830279d
unknown <tiago.o.caetano@gmail.com>
2019-10-16 00:13:15 +0100

    Ficha05 - ex8

--- 2019/Praticas/Ficha05_SQL_Subconsultas/exercicios_ficha05.sql
+++ 2019/Praticas/Ficha05_SQL_Subconsultas/exercicios_ficha05.sql
@@ -22,9 +22,9 @@ WHERE numdep = (SELECT numdep
 SELECT nomedep
 FROM departamento
 WHERE numdep = (SELECT numdep
-                  FROM empregado
-                  GROUP BY numdep
-                  HAVING (AVG(salario) < (SELECT AVG(salario)
+                FROM empregado
+                GROUP BY numdep
+                HAVING (AVG(salario) < (SELECT AVG(salario)
                                           FROM empregado)));
                                           
                                           
@@ -33,10 +33,38 @@ WHERE numdep = (SELECT numdep
 SELECT nomedep
 FROM departamento
 WHERE numdep NOT IN (SELECT numdep
-                      FROM empregado);
+                     FROM empregado);
 
 
 
+/*  Ex. 5   */
+SELECT COUNT(*) AS "Número de Chefes"
+FROM (SELECT DISTINCT(aux.numemp)
+      FROM empregado e JOIN empregado aux ON (e.chefe = aux.numemp));
 
 
-                                          
+
+/*  Ex. 6   */
+SELECT ROUND(MAX(AVG(salario)),2) AS "Maior Salario Médio"
+FROM empregado
+GROUP BY numdep;
+
+
+
+/*  Ex. 7  */
+SELECT COUNT(*) AS "Dept. com mais de 3 Emp."
+FROM (SELECT COUNT(*)
+      FROM empregado
+      GROUP BY numdep
+      HAVING COUNT(*) > 3);
+ 
+      
+      
+/*  Ex. 8  */
+SELECT e.nomeemp , e.numdep, e.salario
+FROM empregado e
+WHERE e.salario > (SELECT AVG(salario)
+                   FROM empregado
+                   WHERE numdep = e.numdep)
+ORDER BY 2 ASC, 3 DESC;
+
